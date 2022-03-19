d184999acff75449066dbfd54af85424d51be778
Evgeny Zakiev <zakiev_e@mail.ru>
2022-03-19 15:33:43 +0300

    initial

---
+++ practical_stepic.py
@@ -0,0 +1,40 @@
+number_1 = int(input(''))
+number_2 = int(input(''))
+result = (3 * (number_1 + number_2) ** 3) + (275 * (number_2 ** 2)) - (127 * number_1) - 41
+print(result)
+
+# number = int(input())
+# print(f'Следующее за числом {number} число: {number + 1} ')
+# print(f'Для числа {number} предыдущее число: {number - 1} ')
+
+number = int(input())
+print('Следующее за числом', number, 'число:', number + 1)
+print('Для числа', number, 'предыдущее число:', number - 1)
+
+monitor = int(input(''))
+system_unit = int(input(''))
+keyboard = int(input(''))
+mouse = int(input(''))
+summa = (monitor + system_unit + keyboard + mouse) * 3
+print(summa)
+
+number_1 = int(input(''))
+number_2 = int(input(''))
+summa = number_1 + number_2
+difference = number_1 - number_2
+multiplication = number_1 * number_2
+print(f'{number_1} + {number_2} = {summa}')
+print(f'{number_1} - {number_2} = {difference}')
+print(f'{number_1} * {number_2} = {multiplication}')
+
+a_1 = int(input(''))
+d = int(input(''))
+n = int(input(''))
+result = a_1 + (d * (n - 1))
+print(result)
+
+
+number_1 = int(input(''))
+print(f'{number_1}---{number_1 * 2}---{number_1 * 3}---{number_1 * 4}---{number_1 * 5}')
+
+
