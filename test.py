c593b4031f8bf1bbdfdc1879150f72b0580d813d
Evgeny Zakiev <zakiev_e@mail.ru>
2022-12-09 16:19:42 +0300

    conditional operator add

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-print(2000 / 4)
-print(2000 / 400)
-print(2000 / 100)
+# def foo(name):
+#     # return " ".join(['Hi', name])
+#     return 'Hi', name
+#
+#
+# foo('Natasha!')
+#
+# from lesson import foo
+#
+# name = 'Natasha!'
+# assert foo(name) == 'Hi Natasha!', 'Ошибка! Тест не пройден.'
+
+print(1 % 2 == 0)
+print(2 % 2 == 0)
+print(1 / 2)
+print(2 / 2)
--- test_stepic.py
+++
@@ -1,6 +0,0 @@
-nums = set(int(input()) for _ in range(3))
-print(nums)
-print([len(nums)])
-print(["Равносторонний", "Равнобедренный", "Разносторонний"][len(nums) - 1])
-
-print([18, 5, 8][2])
