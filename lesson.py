86a47fd03d3e8bb5ed9aaa61635c171d2bc7b60c
Evgeny Zakiev <zakiev_e@mail.ru>
2023-02-24 14:19:17 +0300

    Lists - add

--- lesson.py
+++ lesson.py
@@ -1,9 +1,10 @@
-text = input() # affective
-if "f" not in text:
-    print("-2")
-elif text.count("f") == 1:
-    print("-1")
+text = input().split("-")
+if "".join(text).isdigit():
+    if text[0] == "7":
+       del text[0]
+    if len(text[0]) == 3 and len(text[1]) == 3 and len(text[2]) == 4:
+        print("YES")
+    else:
+        print("NO")
 else:
-    print(text.find("f", text.find("f") + 1))
-
-
+    print("NO")
